303f2423768c82ea1677fc9bcaabde34c98339dd
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-04-21 12:25:58 -0300

    migration de usuário;

---
+++ src/database/migrations/1587482741955-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1587482741955 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
