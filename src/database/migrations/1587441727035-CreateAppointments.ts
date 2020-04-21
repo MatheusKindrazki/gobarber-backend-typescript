e9005d68219690b5b2f65efa8e479fc6bbc330a4
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-04-21 12:21:21 -0300

    criando dados dentro do banco com typeorm

--- src/database/migrations/1587441727035-CreateAppointments.ts
+++ src/database/migrations/1587441727035-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587441727035
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
